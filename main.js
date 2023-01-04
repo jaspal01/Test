a1840b21c6dd380f45f6e262a4451f03b9ee1a11
jaspal01 <73422463+jaspal01@users.noreply.github.com>
2023-01-05 00:37:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 4000;
 
 // Database Connection
 mongoose.set("strictQuery", true);
-console.log(process.env.DB_URL || "mongodb+srv://jaspal:jaspal123@cluster0.0lcwkyb.mongodb.net/test")
-mongoose.connect("mongodb://jaspal:jaspal123@cluster0.0lcwkyb.mongodb.net:27017/test&authSource=admin");
+console.log(process.env.DB_URL || "")
+mongoose.connect("mongodb+srv://jaspal:jaspal123@cluster0.0lcwkyb.mongodb.net/test");
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to database"));
